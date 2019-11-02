e6665909a6113092c93723f5f63ac3a7a0872961
Jordan <knightworks@pm.me>
2019-11-02 16:39:57 -0500

    added helmet mod

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const { env } = require('frontier')
+const helmet = require('helmet')
 
-let port = env.get('PORT') || 3131;
 let server = require('express')()
+let port = env.get('PORT') || 3131;
 let router = require('./router')
+
+server.use(helmet())
+server.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }))
 
 server.use('/api/v1', router)
 server.listen(port, () => console.log(`Auth Server listening on port ${port}!`))
